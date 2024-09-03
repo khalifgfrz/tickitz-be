3b58c880a059cbd2152935fe4a01c707a9115b25
Irsyad Ridho <irsyadridho.r@gmail.com>
2024-09-03 09:15:14 +0700

    added getLink for pagination and updated pagination code 2

--- internal/handlers/movies.handler.go
+++ internal/handlers/movies.handler.go
@@ -328,7 +328,7 @@ func (h *HandlerMovie) GetMovies(ctx *gin.Context) {
 	response := pkg.NewResponse(ctx)
 
 	pageStr := ctx.DefaultQuery("page", "1")
-	limitStr := ctx.DefaultQuery("limit", "2")
+	limitStr := ctx.DefaultQuery("limit", "12")
 
 	page, err := strconv.Atoi(pageStr)
 	if err != nil || page < 1 {
